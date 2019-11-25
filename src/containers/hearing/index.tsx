bc045bc9c80748b9f2e7f726095793fad8e3aac8
Di Wang <wangdicoder@gmail.com>
2019-11-25 23:18:29 +1100

    feat: add hearing page

---
+++ src/containers/hearing/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from "../../components/banner";
+
+export default class Hearing extends React.PureComponent {
+
+  render() {
+    return (
+      <div>
+        <Banner
+          title="Listen to them"
+        />
+      </div>
+    );
+  }
+}
--- src/routers.tsx
+++ src/routers.tsx
@@ -2,10 +2,12 @@ import Document from './containers/document';
 import Communication from './containers/communication';
 import Health from './containers/health';
 import University from './containers/university';
+import Hearing from './containers/hearing';
 
 export default [
   {name: 'Communication', route: '/communication', component: Communication},
   {name: 'Documents', route: '/document', component: Document},
   {name: 'Healthcare System', route: '/healthcare-system', component: Health},
   {name: 'La Trobe ISS', route: '/la-trobe', component: University},
+  {name: 'Hearing', route: '/hearing', component: Hearing},
 ];
