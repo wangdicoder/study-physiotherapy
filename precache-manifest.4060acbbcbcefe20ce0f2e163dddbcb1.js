ad4efc5a32a098d1735220b93f1a81c7e0e4c976
Di Wang <wangdicoder@gmail.com>
2019-11-28 11:11:08 +1100

    Updates

---
+++ precache-manifest.4060acbbcbcefe20ce0f2e163dddbcb1.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdceab565c1de796c68a1c1b834f716b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db1d6cc0c58b7dde304",
+    "url": "/static/css/2.0d558061.chunk.css"
+  },
+  {
+    "revision": "244486c6323f92786ef3",
+    "url": "/static/css/main.19c35f7c.chunk.css"
+  },
+  {
+    "revision": "4db1d6cc0c58b7dde304",
+    "url": "/static/js/2.681dc17d.chunk.js"
+  },
+  {
+    "revision": "244486c6323f92786ef3",
+    "url": "/static/js/main.ca325cab.chunk.js"
+  },
+  {
+    "revision": "e5e07314ad887a95fc58",
+    "url": "/static/js/runtime-main.d56a0b0c.js"
+  },
+  {
+    "revision": "509b17ed4835df09d0e585e0bfada9db",
+    "url": "/static/media/banner_communication.509b17ed.jpg"
+  },
+  {
+    "revision": "f05a96f5eefb10b338baf927ad7f14c1",
+    "url": "/static/media/banner_docs.f05a96f5.jpg"
+  },
+  {
+    "revision": "2b2447fddaaa13c49729dfc9dd835978",
+    "url": "/static/media/banner_feedback.2b2447fd.jpg"
+  },
+  {
+    "revision": "b7f7d4a4c9e158704f0828b770a1be00",
+    "url": "/static/media/banner_healthcare.b7f7d4a4.jpg"
+  },
+  {
+    "revision": "2fb2d6f77bd191ffc553083891f914f5",
+    "url": "/static/media/banner_issue.2fb2d6f7.jpg"
+  },
+  {
+    "revision": "b87a7488d989a7ebfa77b60280ed5605",
+    "url": "/static/media/banner_more-info.b87a7488.jpg"
+  },
+  {
+    "revision": "46523f21735243aafe6ce11c90de6568",
+    "url": "/static/media/bg-hero-slide.46523f21.jpg"
+  },
+  {
+    "revision": "fe6d2dbbbe981b73ea1640b2a561a99b",
+    "url": "/static/media/group.fe6d2dbb.svg"
+  },
+  {
+    "revision": "9264c856e6be96692be99808751c7863",
+    "url": "/static/media/ic_accent.9264c856.svg"
+  },
+  {
+    "revision": "eae10160959d4a92e7a06e5f08d4af84",
+    "url": "/static/media/ic_ask.eae10160.svg"
+  },
+  {
+    "revision": "c8e77f200d998fa047de568604a2fee3",
+    "url": "/static/media/ic_certificate.c8e77f20.svg"
+  },
+  {
+    "revision": "7df2c2caa690319f8f3b2db4caddebcc",
+    "url": "/static/media/ic_form.7df2c2ca.svg"
+  },
+  {
+    "revision": "f25d52228c09b121afbbe53242416b88",
+    "url": "/static/media/ic_formcheck.f25d5222.svg"
+  },
+  {
+    "revision": "b0492f44f8d58bb07ce3b26ae8e5e480",
+    "url": "/static/media/ic_group.b0492f44.svg"
+  },
+  {
+    "revision": "2673cb73bc791ba2b77cbbb5a47c6387",
+    "url": "/static/media/ic_news.2673cb73.svg"
+  },
+  {
+    "revision": "45aa3701b64082bacbf642749b0799e3",
+    "url": "/static/media/ic_paper.45aa3701.svg"
+  },
+  {
+    "revision": "0a91cb64a1cf46d304010822fb3d2c2a",
+    "url": "/static/media/ic_thumb.0a91cb64.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/study-physiotherapy/./precache-manifest.ab1e8292e26c7aa44d0c2768ba737edc.js"
+  "/precache-manifest.4060acbbcbcefe20ce0f2e163dddbcb1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/study-physiotherapy/./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
